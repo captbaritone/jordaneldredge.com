291db4f992dd50550f444a021d12c181c0fcca94
Jordan Eldredge <jordan@jordaneldredge.com>
2020-12-15 21:01:40 -0800

    [nextjs] Handle wildcards

--- nextjs/lib/markdownToHtml.js
+++ nextjs/lib/markdownToHtml.js
@@ -2,7 +2,26 @@ import remark from "remark";
 import html from "remark-html";
 import prism from "remark-prism";
 
+const CONSTANTS = {
+  "site.email": "jordan@jordaneldredge.com",
+  "site.twitter_username": "captbaritone",
+  "site.github_username": "captbaritone",
+  "site.url": "https://jordaneldredge.com",
+};
+
+function preprocess(markdown) {
+  return markdown.replace(/\{\{ ([^}]+) \}\}/g, (_, key) => {
+    const value = CONSTANTS[key];
+    if (value == null) {
+      throw new Error(`Could not find constant value for key "${key}"`);
+    }
+    return value;
+  });
+}
+
 export default async function markdownToHtml(markdown) {
-  const result = await remark().use(html).use(prism).process(markdown);
+  const processedMarkdown = preprocess(markdown);
+
+  const result = await remark().use(html).use(prism).process(processedMarkdown);
   return result.toString();
 }
