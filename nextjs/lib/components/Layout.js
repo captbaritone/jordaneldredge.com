3560c53a6776e31715c66bb98b100240dab94256
Jordan Eldredge <jordan@jordaneldredge.com>
2020-12-16 22:28:07 -0800

    [nextjs] Add favicon

--- nextjs/lib/components/Layout.js
+++ nextjs/lib/components/Layout.js
@@ -10,6 +10,7 @@ export default function Layout({ children, title }) {
           href="https://unpkg.com/prismjs@0.0.1/themes/prism-tomorrow.css"
           rel="stylesheet"
         />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
       </Head>
       <div className="max-w-2xl mx-auto p-5">
         <nav className="flex justify-between flex-col sm:flex-row pb-5">
