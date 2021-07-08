6c0225a520eaa6c1eef6b4041f4032816a367bdb
Jordan Eldredge <jordan@jordaneldredge.com>
2021-07-07 23:33:56 -0700

    Improve og tags for nextjs

--- nextjs/lib/components/Layout.js
+++ nextjs/lib/components/Layout.js
@@ -1,16 +1,30 @@
 import Head from "next/head";
 import Link from "next/link";
+/* TODO
+<meta name="description" content="{% if page.summary %}{{ page.summary }}{% else %}{{ site.description }}{% endif %}">
+{% if page.categories %}<meta name="keywords" content="{{ page.categories | join: ', ' }}">{% endif %}
+<link rel="canonical" href="{{ page.url | replace:'index.html','' | prepend: site.baseurl | prepend: site.url }}">
+
+<!-- Open Graph -->
+<!-- From: https://github.com/mmistakes/hpstr-jekyll-theme/blob/master/_includes/head.html -->
+<meta property="og:description" content="{% if page.summary %}{{ page.summary }}{% else %}{{ site.description }}{% endif %}">
+<meta property="og:url" content="{{ site.url }}{{ page.url }}">
+
+*/
 
 export default function Layout({ children, title }) {
+  const fullTitle = (title ? `${title} / ` : "") + "Jordan Eldredge";
   return (
     <>
       <Head>
-        <title>{(title ? `${title} / ` : "") + "Jordan Eldredge"}</title>
-        <link
-          href="https://unpkg.com/prismjs@0.0.1/themes/prism-tomorrow.css"
-          rel="stylesheet"
-        />
+        <meta property="og:locale" content="en_US" />
+        <title>{fullTitle}</title>
+        <meta property="og:title" content={fullTitle} />
+        <meta name="author" content="Jordan Eldredge" />
+        <meta property="og:site_name" content="Jordan Eldredge" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <meta name="twitter:site" content="@captbaritone" />
+        <meta name="twitter:creator" content="@captbaritone" />
       </Head>
       <div className="max-w-2xl mx-auto p-5">
         <nav className="flex justify-between flex-col sm:flex-row pb-5">
--- nextjs/pages/blog/[slug].js
+++ nextjs/pages/blog/[slug].js
@@ -4,6 +4,37 @@ import * as Api from "../../lib/api";
 import markdownToHtml from "../../lib/markdownToHtml";
 import Layout from "../../lib/components/Layout";
 import GitHubComments from "../../lib/components/GitHubComments";
+import Head from "next/head";
+
+/*
+{% if page.summary_image %}
+{% elsif page.youtube_slug %}
+    <meta property="og:image" content="https://i.ytimg.com/vi/{{ page.youtube_slug }}/maxresdefault.jpg">
+    <meta name="twitter:image" content="https://i.ytimg.com/vi/{{ page.youtube_slug }}/maxresdefault.jpg">
+{% else %}
+    <meta property="og:image" content="{{ "/images/jordan_eldredge.jpg" | prepend: side.baseurl | prepend: site.url }}">
+{% endif %}
+
+{% if page.youtube_slug %}
+    <meta name="twitter:card" content="player">
+    <meta name="twitter:image" content="https://i.ytimg.com/vi/{{ page.youtube_slug }}/hqdefault.jpg">
+    <meta name="twitter:title" content="{% if page.title %}{{ page.title | escape }}{% else %}{{ site.title }}{% endif %}">
+    <meta name="twitter:app:name:iphone" content="YouTube">
+    <meta name="twitter:app:id:iphone" content="544007664">
+    <meta name="twitter:app:name:ipad" content="YouTube">
+    <meta name="twitter:app:id:ipad" content="544007664">
+    <meta name="twitter:app:url:iphone" content="vnd.youtube://www.youtube.com/watch?v={{ page.youtube_slug }}&amp;feature=applinks">
+    <meta name="twitter:app:url:ipad" content="vnd.youtube://www.youtube.com/watch?v={{ page.youtube_slug }}&amp;feature=applinks">
+    <meta name="twitter:app:name:googleplay" content="YouTube">
+    <meta name="twitter:app:id:googleplay" content="com.google.android.youtube">
+    <meta name="twitter:app:url:googleplay" content="https://www.youtube.com/watch?v={{ page.youtube_slug }}">
+    <meta name="twitter:player" content="https://www.youtube.com/embed/{{ page.youtube_slug }}">
+    <meta name="twitter:player:width" content="1280">
+    <meta name="twitter:player:height" content="720">
+{% else %}
+    <meta name="twitter:card" content="summary">
+{% endif %}
+*/
 
 export default function Post({ post }) {
   const router = useRouter();
@@ -12,6 +43,12 @@ export default function Post({ post }) {
   }
   return (
     <Layout title={post.title}>
+      <Head>
+        <meta property="og:type" content="article" />
+        {post.summary_image && (
+          <meta property="og:image" content={post.summary_image} />
+        )}
+      </Head>
       <div className="markdown">
         <h1>{post.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.content }} />
@@ -29,6 +66,7 @@ export async function getStaticProps({ params }) {
     "slug",
     "content",
     "github_comments_issue_id",
+    "summary_image",
   ]);
   const content = await markdownToHtml(post.content || "");
 
