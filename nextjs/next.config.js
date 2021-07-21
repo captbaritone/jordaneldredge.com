c94e633e4b40ab360916a5953e71c5a6de3c8426
Jordan Eldredge <jordan@jordaneldredge.com>
2021-07-20 19:22:29 -0700

    Delete old

--- nextjs/next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  distDir: "nextjs/build",
-  rewrites: [
-    {
-      source: "/projects/:match*",
-      destination: "https://jordaneldredge.com/project/:match*",
-    },
-  ],
-};
