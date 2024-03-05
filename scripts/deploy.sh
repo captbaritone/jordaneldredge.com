9da2fa8fbb2abe0a8586b7a1d1aaa0acf62f10c8
Jordan Eldredge <jordan@jordaneldredge.com>
2024-03-05 15:57:29 -0800

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,19 @@
-yarn build
+# Prompt if git is dirty
+if ! git diff-index --quiet HEAD --; then
+  echo "Git is dirty. Commit your changes before deploying."
+  exit 1
+fi
+echo "Pushing to origin"
+git push origin
+echo "Building"
+pnpm build
+echo "Copying to server"
 scp -r build/* jordan:~/projects/jordaneldredge.com/build
-# On server
-# cd ~/projects/jordaneldredge.com
-# git fetch origin
-# git rebase origin/master
-# yarn
-# pm2 restart jordaneldredge.com
+
+echo "Build complete and copied to server"
+echo "On server"
+echo "cd ~/projects/jordaneldredge.com"
+echo "git fetch origin"
+echo "git rebase origin/master"
+echo "pnpm i"
+echo "pm2 restart jordaneldredge.com"
