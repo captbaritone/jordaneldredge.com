fb38892097fc13d805cca72902d93256fabf3ed1
Jordan Eldredge <jordan@jordaneldredge.com>
2024-04-24 11:47:56 -0400

    Add ecosystem.config.js file to repo

---
+++ ecosystem.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apps : [{
+    name: "jordanledredge.com",
+    script: "node_modules/next/dist/bin/next",
+    args: "start -p 4000",
+    interpreter: "/home/captbaritone/.nvm/versions/node/v20.11.1/bin/node"
+  }]
+}
