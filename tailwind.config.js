eaa6da40355dc7e3928aeba03e186eb8ad962aa8
Jordan Eldredge <jordan@jordaneldredge.com>
2024-07-13 22:56:36 -0700

    Fix spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.js",
+    "./pages/**/*.ts",
+    "./pages/**/*.tsx",
     "./app/**/*.js",
+    "./app/**/*.ts",
+    "./app/**/*.tsx",
     "./lib/components/**/*.js",
+    "./lib/components/**/*.ts",
+    "./lib/components/**/*.tsx",
     "./_pages/*.md",
     "./_posts/*.md",
   ],
