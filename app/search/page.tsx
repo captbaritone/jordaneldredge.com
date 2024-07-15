221b21ce7cf07d5cdd1da3f22ffd0c85cd79af71
Jordan Eldredge <jordan@jordaneldredge.com>
2024-07-14 23:51:39 -0700

    Add back suspense

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import PageImpl from "./pageImpl";
 
 export async function generateMetadata({ searchParams }) {
@@ -6,5 +7,9 @@ export async function generateMetadata({ searchParams }) {
 }
 
 export default function Search() {
-  return <PageImpl />;
+  return (
+    <Suspense fallback={null}>
+      <PageImpl />
+    </Suspense>
+  );
 }
