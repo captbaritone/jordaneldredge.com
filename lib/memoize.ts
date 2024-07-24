3fa30c8f19e209080f58c6d6e30330f764115277
Jordan Eldredge <jordan@jordaneldredge.com>
2024-07-23 22:50:36 -0700

    Stale while revalidate for promises

--- lib/memoize.ts
+++ lib/memoize.ts
@@ -7,6 +7,12 @@ type Options = {
 
 /**
  * Memoizes a function with a cache that expires after a certain amount of time.
+ *
+ * If the function returns a Promise, we use a "stale-while-revalidate" strategy
+ * on the assumption that it's better to never wait on async data if we can help it.
+ * This should mean that even if the site gets infrequent traffic, users should still
+ * experience fast load times which for a largely static site is more important
+ * than it being up-to-date. Eventual consistency is fine.
  */
 export function memoize<F extends (...args: any[]) => any>(
   options: Options,
@@ -24,6 +30,18 @@ export function memoize<F extends (...args: any[]) => any>(
     const value = fn(...args);
     const expires = Date.now() + options.ttl;
     cache.set(key, { value, expires });
+
+    // If the value is a promise, and we already have a cached value, we return
+    // the cached value immediately on the assumption that the cached value
+    // already resolved and it's better to not wait on async data if we can help
+    // it.
+    // The next reader will get the new value.
+    if (cached && value instanceof Promise) {
+      return cached.value;
+    }
+
+    // If the value is _not_ a promise, we return the new value directly since
+    // the work to compute the new value has presumably already been done.
     return value;
   } as F;
 }
