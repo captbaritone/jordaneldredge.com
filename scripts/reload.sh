c6bbf0a2bc7b8d96a62b76133dd80571419bb21d
Jordan Eldredge <jordan@jordaneldredge.com>
2024-07-24 00:52:27 -0400

    Try to get NVM working

--- scripts/reload.sh
+++ scripts/reload.sh
@@ -1,6 +1,11 @@
 # Exit if any command fails
 set -e
 
+
+# Source nvm
+# https://unix.stackexchange.com/a/184512
+. ~/.nvm/nvm.sh
+
 echo "Fetching and rebasing"
 git fetch origin
 git rebase origin/master
