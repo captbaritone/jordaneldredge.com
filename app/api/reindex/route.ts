87618f7cbb1a8dfe5ef1f4c8b2b01acfbd1808a5
Jordan Eldredge <jordan@jordaneldredge.com>
2024-11-30 22:03:12 -0800

    Fix import

--- app/api/reindex/route.ts
+++ app/api/reindex/route.ts
@@ -1,10 +1,10 @@
-import * as Search from "../../../lib/search";
+import * as Indexable from "../../../lib/data/Indexable";
 import { revalidatePath } from "next/cache";
 
 export const dynamic = "force-dynamic";
 
 export async function GET() {
-  await Search.reindex();
+  await Indexable.reindex({ force: false });
   revalidatePath("/");
   return new Response(JSON.stringify({ status: "ok" }));
 }
