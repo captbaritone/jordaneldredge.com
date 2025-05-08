3c3098abcbe9cc8d80ae42691173d8e3f6abe4a4
Jordan Eldredge <jordan@jordaneldredge.com>
2025-05-07 19:37:17 -0700

    Fix catch up migration?

--- migrations/001-006-catch-up.ts
+++ migrations/001-006-catch-up.ts
@@ -22,14 +22,6 @@ export async function up() {
       UNIQUE (page_type, slug)
     );
 
-    CREATE TABLE IF NOT EXISTS 'content_fts_data' (id INTEGER PRIMARY KEY, block BLOB);
-
-    CREATE TABLE IF NOT EXISTS 'content_fts_idx' (segid, term, pgno, PRIMARY KEY (segid, term)) WITHOUT ROWID;
-
-    CREATE TABLE IF NOT EXISTS 'content_fts_docsize' (id INTEGER PRIMARY KEY, sz BLOB);
-
-    CREATE TABLE IF NOT EXISTS 'content_fts_config' (k PRIMARY KEY, v) WITHOUT ROWID;
-
     CREATE TABLE tts (
       id INTEGER PRIMARY KEY,
       r2_key TEXT NOT NULL,
