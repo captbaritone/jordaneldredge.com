054aac4436acc2b1dde7c76b37f2548f8f3ecafb
Jordan Eldredge <jordan@jordaneldredge.com>
2025-05-22 14:03:35 -0700

    Enable view transitions

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@
 module.exports = {
   experimental: {
     scrollRestoration: true,
+    viewTransition: true,
   },
   // // https://github.com/shikijs/next-shiki
   serverExternalPackages: ["shiki", "vscode-oniguruma"],
