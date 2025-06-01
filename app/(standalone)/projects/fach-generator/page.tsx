d92957d6023bbbddc7a41e69486adbadd88a3c66
Jordan Eldredge <jordan@jordaneldredge.com>
2025-06-01 16:19:05 -0700

    Force dynamic fach generation

--- app/(standalone)/projects/fach-generator/page.tsx
+++ app/(standalone)/projects/fach-generator/page.tsx
@@ -1,5 +1,16 @@
 import FachPage from "./fach";
 
+export const metadata = {
+  title: "Funny Fach Generator",
+  description: "Generate a funny random Fach name with a click",
+  twitter: {
+    title: "Funny Fach Generator",
+    description: "Generate a funny random Fach name with a click",
+  },
+};
+
+export const dynamic = "force-dynamic";
+
 export default function Page() {
   const baseSeed = Math.random() * 1000000; // Generate a random seed for the Fach page
   return <FachPage baseSeed={baseSeed} />;
